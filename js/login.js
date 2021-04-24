4c5fd225a3dc8caa3d4557cb7516915e51f9b07e
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-04-24 14:11:15 -0300

    Login pronto, implementar o css

---
+++ js/login.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#btnLogin").click(function () {
+        let $user = $("#user").val();
+        let $pwd = $("#pwd").val();
+        if ($user && $pwd) {
+            $.getJSON("../json/users.json", function ($registros) {
+                if($registros.usuarios.filter($usuario => $usuario.user == $user && $usuario.pwd == $pwd).length > 0)
+                    window.open("moleGame.html", "_self");
+                else
+                alert("Usuário ou senha incorretos, tente novamente ou cadastre-se!")              
+            });
+        }
+        else
+            alert("Digite o usuário e a senha!");
+
+    });
+});
